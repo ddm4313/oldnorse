24b0c34abd1e5e8cf1861a7b2a26a6345d09996e
ddm50 <53798547+ddm50@users.noreply.github.com>
2019-08-29 21:13:00 +0200

    Add files via upload

---
+++ oldnorse.py
@@ -0,0 +1,13 @@
+import requests, pyfiglet, random
+from bs4 import BeautifulSoup
+from colorama import Fore
+
+def get_norse_name():
+    norse_site = requests.get(f"https://babynames.net/all/old-norse?page={random.randint(1, 9)}")
+    soup = BeautifulSoup(norse_site.text, features="lxml")
+    norse_names = soup.findAll("span", attrs={"class": "result-name"})
+
+    norse_list = []
+    for name in norse_names:
+        norse_list.append(name.text)
+    print(random.choice(norse_list))
